ed1de9f198563e364e971857c3cb1a171f8b98c0
varun-developer-public <varun@btreesystems.com>
2025-07-05 17:24:32 +0530

    Update static file settings and remove unused dependency from requirements

--- ArabDataMarketPlace/settings.py
+++ ArabDataMarketPlace/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/"staticfiles"]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR/"static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
